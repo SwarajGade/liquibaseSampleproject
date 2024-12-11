d5cb99ed36eb06ec394f9d3bffd04261e67328b3
swarajGade3 <swarajg@voltacabs.com>
2024-12-11 10:07:29 +0530

    added new project

---
+++ LiquibaseSample/src/test/java/com/LiquibaseSample/LiquibaseSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LiquibaseSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
